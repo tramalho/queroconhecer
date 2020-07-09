d74aa910063c1563ed16168c8ad5f9f9d7dac560
tramalho <thiagofacul@gmail.com>
2020-07-09 14:07:14 -0300

    97. CLGeocoder: Pesquisando cidades e locais diversos

---
+++ QueroConhecer/Place.swift
@@ -0,0 +1,51 @@
+//
+//  Place.swift
+//  QueroConhecer
+//
+//  Created by Thiago Antonio Ramalho on 09/07/20.
+//  Copyright © 2020 Tramalho. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct Place {
+    
+    let name: String
+    let latitude: CLLocationDegrees
+    let longitude: CLLocationDegrees
+    let address: String
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static func getFormattedAddress(with placemark: CLPlacemark) -> String {
+        var address = ""
+        
+        if let street = placemark.thoroughfare {
+            address += street
+        }
+        if let number = placemark.subThoroughfare {
+            address += " \(number)"
+        }
+        if let neighborhood = placemark.subLocality {
+            address += ", \(neighborhood)"
+        }
+        if let city = placemark.locality {
+            address += "\n\(city)"
+        }
+        if let state = placemark.administrativeArea {
+            address += " - \(state)"
+        }
+        if let postalCode = placemark.postalCode {
+            address += "\nCEP:\(postalCode)"
+        }
+        if let country = placemark.country {
+            address += "\n\(country)"
+        }
+        
+        return address
+    }
+    
+}
--- QueroConhecer/PlacesFinderViewController.swift
+++ QueroConhecer/PlacesFinderViewController.swift
@@ -13,7 +13,7 @@ class PlacesFinderViewController: UIViewController {
 
     @IBOutlet weak var textfieldCity: UITextField!
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var viewLoading: UIActivityIndicatorView!
+    @IBOutlet weak var viewLoading: UIView!
     @IBOutlet weak var activeIndicatorLoading: UIActivityIndicatorView!
     
     override func viewDidLoad() {
@@ -21,8 +21,28 @@ class PlacesFinderViewController: UIViewController {
     }
     
     @IBAction func findCity(_ sender: UIButton) {
+        if let address = textfieldCity.text {
+            textfieldCity.resignFirstResponder()
+            loading(show: true)
+            let geo = CLGeocoder()
+            geo.geocodeAddressString(address) { (placemarks, error) in
+                self.loading(show: false)
+                guard let place = placemarks?.first else {return}
+                print(Place.getFormattedAddress(with: place))
+            }
+        }
     }
 
+    private func loading(show: Bool) {
+    
+        self.viewLoading.isHidden = !show
+        
+        if(show) {
+            self.activeIndicatorLoading.startAnimating()
+        } else {
+            self.activeIndicatorLoading.stopAnimating()
+        }
+    }
     
     @IBAction func close(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
